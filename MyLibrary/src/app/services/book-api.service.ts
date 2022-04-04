dbb5095feff9355e91cd320419229c9eee37552e
StevenK418 <stevenkelly418@gmail.com>
2022-04-04 13:55:53 +0100

    => Added new book api service.

---
+++ MyLibrary/src/app/services/book-api.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import {AngularFirestoreCollection, AngularFirestore} from "@angular/fire/compat/firestore";
+import { IBook } from '../interfaces/book';
+
+
+@Injectable()
+
+
+export class BookApiService {
+
+  booksDataCollection:AngularFirestoreCollection<IBook>;
+  booksData?:Observable<IBook[]>;
+
+  allBooksData?:IBook[];
+
+  errorMessage?:string;
+
+  constructor(private _http:HttpClient, private _afs:AngularFirestore) 
+  { 
+    this.booksDataCollection =_afs.collection<IBook>("books_data");
+  }
+
+  getBookData():Observable<IBook[]>
+  {
+    //Connect to the db
+    this.booksData = this.booksDataCollection.valueChanges({idField:`id`});
+    this.booksData?.subscribe(
+      data=> console.log("getBooksData" + JSON.stringify(data))
+    )
+
+    //Return the book data from the database
+    return this.booksData;
+  }
+
+  //Gracefully handle any errors
+  private handleError (err:HttpErrorResponse)
+   {
+     console.log('BookApiService: ' + err.message);
+     return throwError(err.message);
+   }
+}
--- MyLibrary/src/environments/environment.ts
+++ MyLibrary/src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyB2CY8xca8cOsbrmSGnBPmCw7GgjjWg4NU",
+  authDomain: "mylibrary-7d77c.firebaseapp.com",
+  projectId: "mylibrary-7d77c",
+  storageBucket: "mylibrary-7d77c.appspot.com",
+  messagingSenderId: "634454569623",
+  appId: "1:634454569623:web:45683bf5e31a175283efd2"
 };
 
 /*
