b669b65255686a0d1601a777d0dfacaa2d57123c
StevenK418 <stevenkelly418@gmail.com>
2022-04-04 10:52:42 +0100

    => Added new Display-Book component.

--- MyLibrary/src/app/app.module.ts
+++ MyLibrary/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DisplayBookComponent } from './display-book/display-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayBookComponent
   ],
   imports: [
     BrowserModule,
